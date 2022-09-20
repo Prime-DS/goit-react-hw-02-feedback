cb97e25d66b98124f7f5cecd5e8c564d33e202b3
Prime-DS <prime.up17@gmail.com>
2022-09-20 12:13:56 +0300

    add temlate

--- src/components/FriendList/FriendList.styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-export const FriendsList = styled.ul`
-margin-left:auto;
-margin-right:auto;
-width: 150px;
-    display:flex;
-    flex-direction:column;
-`;
--- src/components/TransactionHistory/TransactionHistory.styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled";
-
-export const HistoryTable = styled.table`
-    width: 800px;
-    margin-left:auto;
-    margin-right:auto;
-    border: 1px solid;
-    color:blue;
-`;
